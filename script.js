46f8108652f102cfb9a0648b90ac4f9cda1f420c
Ganeshbabu7 <110327597+Ganeshbabu7@users.noreply.github.com>
2022-09-23 17:41:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Difference b/w document and window objects
+
+// Document :
+// 1. It represent HTML document
+// 2. It is object of window property
+// 3. Tags, elements & attributes are part of the document
+// 4. Syntax : Document.property name
+// 5. Document.title will return the title of th e document
+
+// Window Objects
+// 1. It represent browser window
+// 2. It is the object of browser
+// 3. Variables, object & functions are part of the browser
+// 4. Syntax : Window.property name
+// 5. Window.innerheight will return the height of the content area.
+
+
